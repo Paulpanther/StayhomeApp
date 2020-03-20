7bdbb3b19c9b5afe9d99398465751b04dba13856
Paul Methfessel <paul.methfessel@web.de>
2020-03-20 23:33:09 +0100

    Stump broadcast receiver

--- app/src/main/java/de/wvvh/stayhomeapp/MainActivity.kt
+++ app/src/main/java/de/wvvh/stayhomeapp/MainActivity.kt
@@ -1,12 +1,26 @@
 package de.wvvh.stayhomeapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.content.IntentFilter
+import android.net.wifi.WifiManager
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        searchWifi()
+        WifiChangeReceiver.registerCallback(applicationContext)
+        val intentFilter = IntentFilter()
+        intentFilter.addAction(WifiManager.SUPPLICANT_CONNECTION_CHANGE_ACTION)
+    }
+
+    private fun searchWifi() {
+        val wifi = applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
+        Log.d("Bla", wifi.connectionInfo.networkId.toString())
     }
 }
