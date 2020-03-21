0175c709e4cdd48cf19985193da3b360aee0d1d8
Antonius Naumann <antonius.naumann@student.hpi.de>
2020-03-22 00:14:06 +0100

    Adds UI detail card for achievements

--- app/src/main/java/de/wvvh/stayhomeapp/ui/main/achievements/AchievementFragment.kt
+++ app/src/main/java/de/wvvh/stayhomeapp/ui/main/achievements/AchievementFragment.kt
@@ -4,11 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import de.wvvh.stayhomeapp.R
 import de.wvvh.stayhomeapp.achievements.AchievementStore
+import de.wvvh.stayhomeapp.achievements.IAchievement
 import de.wvvh.stayhomeapp.ui.main.TabFragment
+import kotlinx.android.synthetic.main.fragment_main_achievement.*
 
 /**
  * @author Antonius Naumann
@@ -22,7 +26,27 @@ class AchievementFragment: TabFragment() {
     ): View? {
         val root = inflater.inflate(R.layout.fragment_main_achievement, container, false)
         val achievementList = root.findViewById<RecyclerView>(R.id.achievement_view)
-        achievementList.adapter = AchievementAdapter(AchievementStore.achievements)
+        val detailView = root.findViewById<View>(R.id.big_card)
+        detailView.setOnClickListener { detailView.visibility = View.GONE }
+        val clickListener = object: AchievementAdapter.OnAchievementClickedListener {
+            override fun onClick(
+                achievement: IAchievement,
+                viewHolder: AchievementAdapter.AchievementViewHolder) {
+
+                detailView.visibility = View.VISIBLE;
+
+                val icon = detailView.findViewById<ImageView>(R.id.achievement_icon)
+                val title = detailView.findViewById<TextView>(R.id.achievement_title)
+                val desc = detailView.findViewById<TextView>(R.id.achievement_description)
+                val explanation = detailView.findViewById<TextView>(R.id.achievement_explanation)
+
+                icon.setImageResource(achievement.imageResource)
+                title.setText(achievement.titleResource)
+                desc.setText(achievement.descriptionResource)
+                explanation.setText(achievement.explanationResource)
+            }
+        }
+        achievementList.adapter = AchievementAdapter(AchievementStore.achievements, clickListener)
         achievementList.layoutManager = GridLayoutManager(
             context,
             2,
