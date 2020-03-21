4561aab58b1b714b681d362b916167650bcf4435
Ice <louis.haub@googlemail.com>
2020-03-21 22:30:20 +0100

    Added temporary redirect

--- app/src/main/java/de/wvvh/stayhomeapp/ui/MainActivity.kt
+++ app/src/main/java/de/wvvh/stayhomeapp/ui/MainActivity.kt
@@ -1,14 +1,17 @@
 package de.wvvh.stayhomeapp.ui
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT
 import androidx.viewpager.widget.ViewPager
 import com.google.android.material.tabs.TabLayout
+import de.wvvh.stayhomeapp.InitialLaunch
 import de.wvvh.stayhomeapp.R
 import de.wvvh.stayhomeapp.achievements.AchievementModules
 import de.wvvh.stayhomeapp.achievements.AchievementStore
 import de.wvvh.stayhomeapp.ui.main.SectionsPagerAdapter
+import de.wvvh.stayhomeapp.wifi.WifiHelper
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,6 +23,7 @@ class MainActivity : AppCompatActivity() {
             supportFragmentManager,
             BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT)
         val viewPager: ViewPager = findViewById(R.id.view_pager)
+        startActivity(Intent(this, InitialLaunch::class.java))
         viewPager.adapter = sectionsPagerAdapter
         val tabs: TabLayout = findViewById(R.id.tabs)
         tabs.setupWithViewPager(viewPager)
