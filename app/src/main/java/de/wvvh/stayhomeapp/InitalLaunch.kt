4be4dcc4f89581aed2153821aa99b324dd116f60
Ice <louis.haub@googlemail.com>
2020-03-21 18:45:22 +0100

    First sketch for Inital Launch UI

---
+++ app/src/main/java/de/wvvh/stayhomeapp/InitalLaunch.kt
@@ -0,0 +1,31 @@
+package de.wvvh.stayhomeapp
+
+import android.annotation.SuppressLint
+import android.app.ActionBar
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.Settings
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+
+class InitalLaunch : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inital_launch)
+        supportActionBar?.setTitle(R.string.home_wifi)
+    }
+
+    public fun updateWifi(){
+        var tv: TextView = findViewById<TextView>(R.id.wifi_name)
+        tv.setText(Math.random().toString())
+    }
+    public fun openWiFi(view: View){
+        startActivity( Intent(Settings.ACTION_WIFI_SETTINGS));
+    }
+    override fun onResume(){
+        super.onResume()
+        updateWifi()
+    }
+}
